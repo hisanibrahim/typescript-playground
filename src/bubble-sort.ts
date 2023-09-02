f2f04a8e7bd84a5125dc8dca4662b9806c494816
Hisan Ibrahim <hisanibrahim@gmail.com>
2023-09-02 17:13:33 +0530

    remove comments for bubble sort

--- src/bubble-sort.ts
+++ src/bubble-sort.ts
@@ -1,29 +1,16 @@
-// const arr: string[] = [
-//   "Naseema",
-//   "Ibrahim",
-//   "Hisan",
-//   "Irfan",
-//   "1",
-//   "Nasrin",
-//   "Shadin",
-// ];
 const arr: string[] = ["5", "3", "2", "4", "1", "6", "9", "8", "7"];
 
 const runBubbleSort = (arr: string[]): void => {
   const n = arr.length;
   for (let i = 0; i < n - 1; i++) {
-    // console.log(`i = ${i} condition= ${i} < ${n - 1}`);
     let swapped = false;
     for (let j = 0; j < n - i - 1; j++) {
-      //   console.log(`  j = ${j} condition= ${j} < ${n - i - 1}`);
       if (arr[j] > arr[j + 1]) {
         const swap = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = swap;
         swapped = true;
-        // console.log("    swapped", arr[j], arr[j + 1]);
       }
-      // else console.log("    not swapped", arr[j], arr[j + 1]);
     }
     if (!swapped) break;
   }
