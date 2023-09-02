ce3a2e2dfce20c7e54e4bb77aaaa11c1f56b3dac
Hisan Ibrahim <hisanibrahim@gmail.com>
2023-09-02 17:14:06 +0530

    sort method mutates original array

--- src/sort.ts
+++ src/sort.ts
@@ -8,12 +8,12 @@ const arr: (number | string)[] = [
   "Shadin",
 ];
 
-const sorted = arr.sort((a, b): number => {
-  if (a < b) return 1; // Return a positive value to place 'a' after 'b'
-  if (a > b) return -1; // Return a negative value to place 'a' before 'b'
+arr.sort((a, b): number => {
+  if (a > b) return 1; // Return a positive value to place 'a' after 'b'
+  if (a < b) return -1; // Return a negative value to place 'a' before 'b'
   return 0; // Return 0 to keep the order unchanged
 });
 
-console.log(JSON.stringify(sorted, null, 2));
+console.log(JSON.stringify(arr, null, 2));
 
 export {};
